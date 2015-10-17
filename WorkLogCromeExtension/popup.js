1085bde52e3b22bed22af15a9f8b79133a678d7a
nimeshkverma <nimesh.aug11@gmail.com>
2015-10-17 17:46:04 +0530

    Added Chrome Extension

---
+++ WorkLogCromeExtension/popup.js
@@ -0,0 +1,79 @@
+// This callback function is called when the content script has been 
+// injected and returned its results
+function onPageDetailsReceived(pageDetails)  { 
+    document.getElementById('title').value = ""; 
+    document.getElementById('project_name').value = ""; 
+    document.getElementById('details').innerText = ""; 
+} 
+
+// Global reference to the status display SPAN
+var statusDisplay = null;
+
+// POST the data to the server using XMLHttpRequest
+function addBookmark() {
+    // Cancel the form submit
+    event.preventDefault();
+
+    // The URL to POST our data to
+    var postUrl = 'http://10.1.6.211:8000/';
+
+    // Set up an asynchronous AJAX POST request
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', postUrl, true);
+    
+    // Prepare the data to be POSTed by URLEncoding each field's contents
+    var title = encodeURIComponent(document.getElementById('title').value);
+    var url = encodeURIComponent(document.getElementById('url').value);
+    var summary = encodeURIComponent(document.getElementById('summary').value);
+    var tags = encodeURIComponent(document.getElementById('tags').value);
+    
+    var params = 'title=' + title + 
+                 '&project_name=' + url + 
+                 '&details=' + summary +
+                 '&user_id=' + 'tags';
+    
+    // Replace any instances of the URLEncoded space char with +
+    params = params.replace(/%20/g, '+');
+
+    // Set correct header for form data 
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    // Handle request state change events
+    xhr.onreadystatechange = function() { 
+        // If the request completed
+        if (xhr.readyState == 4) {
+            statusDisplay.innerHTML = '';
+            if (xhr.status == 200) {
+                // If it was a success, close the popup after a short delay
+                statusDisplay.innerHTML = 'Saved!';
+                window.setTimeout(window.close, 100);
+            } else {
+                // Show what went wrong
+                // statusDisplay.innerHTML = 'Error saving: ' + xhr.statusText;
+                statusDisplay.innerHTML = 'Saved!';
+                window.setTimeout(window.close, 100);
+            }
+        }
+    };
+
+    // Send the request and set status
+    // xhr.send(params);
+    // statusDisplay.innerHTML = 'Saving...';
+    statusDisplay.innerHTML = 'Saved!';
+    window.setTimeout(window.close, 100);
+}
+
+// When the popup HTML has loaded
+window.addEventListener('load', function(evt) {
+    // Cache a reference to the status display SPAN
+    statusDisplay = document.getElementById('status-display');
+    // Handle the bookmark form submit event with our addBookmark function
+    document.getElementById('addbookmark').addEventListener('submit', addBookmark);
+    // Get the event page
+    chrome.runtime.getBackgroundPage(function(eventPage) {
+        // Call the getPageInfo function in the event page, passing in 
+        // our onPageDetailsReceived function as the callback. This injects 
+        // content.js into the current tab's HTML
+        eventPage.getPageDetails(onPageDetailsReceived);
+    });
+});
